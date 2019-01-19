c50158ae2a9cd443e219aa0826dab2b46fb71c60
engcompaulo <engcompaulo@gmail.com>
2019-01-19 08:51:56 +0000

    matrizes

--- introducaoR.R
+++ introducaoR.R
@@ -1,18 +1,18 @@
-# Comentário é com # tralha.
+# Coment?rio ? com # tralha.
 
-# Declaração de variáveis é parecido com Python e JavaScript.
+# Declara??o de vari?veis ? parecido com Python e JavaScript.
 
-a<- 3                                                   # O sinal de atribuição pode ser "<-" sem aspas.
-b=  4                                                   # O sinal de atribuição também pode ser o de igual "=" também sem aspas.
+a<- 3                                                   # O sinal de atribui??o pode ser "<-" sem aspas.
+b=  4                                                   # O sinal de atribui??o tamb?m pode ser o de igual "=" tamb?m sem aspas.
 
 a                                                       # Vai imprimir o valor de a.
 b                                                       # Vai imprimiro o valor de b.
 
-# Ctrl + Enter executa a linha de código em que o cursor esta.
+# Ctrl + Enter executa a linha de c?digo em que o cursor esta.
 
-# Para executar na linha de comando é com o comando Rscript <nomeDoArquivo.R>
+# Para executar na linha de comando ? com o comando Rscript <nomeDoArquivo.R>
 
-# Segue exemplos de outras atribuições e R
+# Segue exemplos de outras atribui??es e R
 
 numero_inteiro<- 10
 numero_real<- 10.5
@@ -27,36 +27,36 @@ caracter                                                # Saida: "P"
 
 # Exemplo de sobrescrita
 
-numero_real<- 12                                        # Atribuição de interiro em uma variavel que era real
-numero_real                                             # A saida será um número inteiro: 12
+numero_real<- 12                                        # Atribui??o de interiro em uma variavel que era real
+numero_real                                             # A saida ser? um n?mero inteiro: 12
 
-# Declaração de vetor
-# Vetor de números inteiros
+# Declara??o de vetor
+# Vetor de n?meros inteiros
 vetor_numeros_inteiros = c(5, 4, 7, 8, 4, 9, 6, 5)
 vetor_numeros_inteiros                                  # Saida: 5 4 7 8 4 9 6 5
 
-# Vetor de números reais
-vetor_numeros_reais = c(4.5, 78.4,85.96, 4.2, 1.454746) # É valido resaltar que o simbolo "." (ponto) representa os números reais.
+# Vetor de n?meros reais
+vetor_numeros_reais = c(4.5, 78.4,85.96, 4.2, 1.454746) # ? valido resaltar que o simbolo "." (ponto) representa os n?meros reais.
 vetor_numeros_reais                                     # Saida: 4.500000 78.400000 85.960000  4.200000  1.454746 
 
 # Vetor de strings (literal)
-vetor_literal = c("Eng", "Com", "Paulo")                # Não pode esquecer de colocar aspas para indicar string.
+vetor_literal = c("Eng", "Com", "Paulo")                # N?o pode esquecer de colocar aspas para indicar string.
 vetor_literal                                           # Saida: "Eng"   "Com"   "Paulo"
 
-# Mostrar apenas um valor de um vetor, basta indicar a posição dentro de colchetes.
+# Mostrar apenas um valor de um vetor, basta indicar a posi??o dentro de colchetes.
 # Exemplo: Mostra o terceiro elemento do vetor "vetor_literal".
 vetor_literal[3]                                       # Saida: "Paulo"
 vetor_literal[1]                                       # Saida: "Eng"
 
 
-# Funções built-in do R
-numero_inteiro<-7                                      # Nova atribuição para numero_inteiro no valor 7.
+# Fun??es built-in do R
+numero_inteiro<-7                                      # Nova atribui??o para numero_inteiro no valor 7.
 
 log(numero_inteiro)                                    # Aqui calcula o logaritmo de numero_inteiro.     Saida: 1.94591
 sqrt(numero_inteiro)                                   # Aqui calcula a raiz quadrada de numero_inteiro. Saida: 2.645751
 factorial(numero_inteiro)                              # Aqui calcula o fatorial de numero_inteiro. Saida: 5040
 
-length(vetor_literal)                                  # Com esta função é possivel verificar o tamanho do vetor(quantidade de elementos). 
+length(vetor_literal)                                  # Com esta fun??o ? possivel verificar o tamanho do vetor(quantidade de elementos). 
 
 
 
@@ -65,12 +65,20 @@ length(vetor_literal)                                  # Com esta função é possi
 
 
 
+# CriaÃ§Ã£o de matriz
+
+A<- matrix(1:20, 4, 5)
+A
+
+A<- array(1:20, dim=c(4, 5, 1))
+A
 
 
+A<- array(1:20, dim=c(4, 5, 4))
+A
 
 
-
-
+# Array Ã© coleÃ§Ã£o de matrizes
 
 
 
